56a7cd0825575bf67c45ecfc0885027b3449a579
Dharmik Bhadra <61378660+theCodeTeen@users.noreply.github.com>
2022-11-07 18:40:26 +0530

    consoles added

--- controllers/redirectController.js
+++ controllers/redirectController.js
@@ -2,16 +2,20 @@ const Link = require("../models/Link");
 
 exports.redirection = async (req, res, next) => {
   try {
+    console.log("redirection ...");
     if (!req.params.id) {
       return next();
     }
+    console.log("id=" + req.params.id);
 
     const link = await Link.findOne({ _id: req.params.id });
 
     if (!link) {
       return next();
     }
+    console.log("link from db=" + link.link);
 
+    console.log("finalling redirecting");
     return res.redirect(link.link);
   } catch (err) {
     console.log(err);
